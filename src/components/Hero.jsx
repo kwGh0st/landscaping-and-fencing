aed31147b185db1d92ffa3e094c6aacf301dfcb3
Karol Programuje <karol341654@gmail.com>
2024-03-06 15:58:50 +0100

    HeroComponent and SectionWrapper

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,22 @@
 import React from "react";
+import { styles } from "../styles";
 import gardenImage from "../assets/herobg.jpg";
 const Hero = () => {
   return (
     <div
-      className="relative h-screen bg-cover bg-center"
+      id="home"
+      className="relative h-screen w-full bg-cover bg-center"
       style={{ backgroundImage: `url(${gardenImage})` }}
     >
       <div className="absolute inset-0 bg-black opacity-40"></div>
       <div className="absolute inset-0 flex items-center justify-center">
-        <div className="text-center text-white">
-          <h1 className="text-4xl md:text-5xl font-bold mb-4">
-            Landscaping&Fencing
+        <div className="text-center text-white font-serif">
+          <h1 className={`${styles.heroHeadText} flex flex-col`}>
+            <span>DW</span> Landscaping&Fencing
           </h1>
-          <p className="text-lg md:text-xl mb-8">
-            Tworzymy piękne ogrody i solidne ogrodzenia, spełniając Twoje
-            marzenia o wymarzonej przestrzeni.
+          <p className={`${styles.heroSubText} mt-2 text-white`}>
+            We create beautiful gardens and sturdy fences, bringing your dreams
+            of an ideal space to life.
           </p>
         </div>
       </div>
