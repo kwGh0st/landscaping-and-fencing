ab5da35a775a4656eababe93b4d9373dc701538e
Karol Programuje <karol341654@gmail.com>
2024-03-12 15:54:34 +0100

    Adding Projects section

---
+++ src/assets/projects/index.js
@@ -0,0 +1,102 @@
+import project1Image1 from "./project1-1.jpg";
+import project1Image2 from "./project1-2.jpg";
+import project1Image3 from "./project1-3.jpg";
+import project1Image4 from "./project1-4.jpg";
+
+import project2Image1 from "./project2-1.jpg";
+import project2Image2 from "./project2-2.jpg";
+import project2Image3 from "./project2-3.jpg";
+import project2Image4 from "./project2-4.jpg";
+
+import project3Image1 from "./project3-1.jpg";
+import project3Image2 from "./project3-2.jpg";
+import project3Image3 from "./project3-3.jpg";
+import project3Image4 from "./project3-4.jpg";
+import project3Image5 from "./project3-5.jpg";
+
+import project4Image1 from "./project4-1.jpg";
+import project4Image2 from "./project4-2.jpg";
+import project4Image3 from "./project4-3.jpg";
+import project4Image4 from "./project4-4.jpg";
+import project4Image5 from "./project4-5.jpg";
+
+import project5Image1 from "./project5-1.jpg";
+import project5Image2 from "./project5-2.jpg";
+import project5Image3 from "./project5-3.jpg";
+import project5Image4 from "./project5-4.jpg";
+import project5Image5 from "./project5-5.jpg";
+
+export const projects = [
+  {
+    id: 1,
+    description: [
+      "New patio completed by DW Landscaping and Fencing Ltd.",
+      "# Silver Grey",
+      "# Natural Stone Slabs",
+    ],
+    images: [project1Image1, project1Image2, project1Image3, project1Image4],
+  },
+  {
+    id: 2,
+    description: [
+      "Works completed by DW in Norton Disney.",
+      "# New patios, steps and paths",
+      "# Indian Sandstone",
+    ],
+    images: [project2Image1, project2Image2, project2Image3, project2Image4],
+  },
+  {
+    id: 3,
+    description: [
+      "Large planter built by DW Landscaping and Fencing Ltd. recently in Lincoln. ",
+      "Materials used: ",
+      "# Tanalised timber sleepers ",
+      "# Tanalised timber posts and rails ",
+      "# Timber lock screws ",
+      "# Postmix ",
+      "# Membrane ",
+      "# Grade one topsoil ",
+      "# Diamond trellis ",
+    ],
+    images: [
+      project3Image1,
+      project3Image2,
+      project3Image3,
+      project3Image4,
+      project3Image5,
+    ],
+  },
+  {
+    id: 4,
+    description: [
+      "Tanalised decking completed in Burton Waters.",
+      "# Work carried out by DW.",
+      "# Materials supplied by Listers Timber and Builders Merchants.",
+    ],
+    images: [
+      project4Image1,
+      project4Image2,
+      project4Image3,
+      project4Image4,
+      project4Image5,
+    ],
+  },
+  {
+    id: 5,
+    description: [
+      "Another landscaping project completed by DW, this time in Navenby for lovely customers. ",
+      "# Autumn Brown Natural Stone Slabs",
+      "# Oak Sleepers Steps",
+      "# Large planter",
+      "# New lawn area and Summerhouse base with existing slabs",
+      "# All materials delivered by Listers Timber and Builders Merchants",
+    ],
+    images: [
+      project5Image1,
+      project5Image2,
+      project5Image3,
+      project5Image4,
+      project5Image5,
+    ],
+  },
+];
---
+++ src/components/Projects.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/free-mode";
+import { Carousel } from "@material-tailwind/react";
+import { projects } from "../assets/projects";
+import { styles } from "../styles";
+import { projectsBg, facebook, instagram } from "../assets";
+
+const ProjectSlider = ({ images }) => {
+  return (
+    <Carousel
+      className="w-full md:w-4/5 h-full rounded-b-lg"
+      breakpoints={{
+        600: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      }}
+    >
+      {images.map((image, index) => (
+        <div key={index} className="w-full h-full">
+          <img className="object-cover" src={image} alt="" />
+        </div>
+      ))}
+    </Carousel>
+  );
+};
+
+const Projects = () => {
+  return (
+    <div className="flex flex-col w-full items-center gap-8 bg-gray-100 text-center">
+      <div
+        className="h-screen relative w-full bg-cover bg-center "
+        style={{ backgroundImage: `url(${projectsBg})` }}
+      >
+        <div className="absolute inset-0 bg-black opacity-40"></div>
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="text-center text-white font-serif">
+            <h1 className={`${styles.heroHeadText} flex flex-col`}>Projects</h1>
+          </div>
+        </div>
+      </div>
+      <p className="mt-12 mb-4 text-2xl italic font-serif max-w-6xl px-6">
+        Some of the projects completed by <br />
+        <span className="italic underline font-bold">
+          DW Landscaping & Fencing.
+          <br />
+        </span>
+        For more please visit:
+        <div className="flex flex-col w-full justify-center items-center mt-8">
+          <div className="flex flex-row justify-center gap-8">
+            <a
+              className="hover:rotate-45 duration-300"
+              href="https://www.facebook.com/waldemar.wojtacha"
+              target="_blank"
+            >
+              <img className="w-12 h-12" src={facebook} alt="facebook" />
+            </a>
+            <a
+              className="hover:rotate-45 duration-300"
+              href="https://www.instagram.com/wojtachawaldemar/"
+              target="_blank"
+            >
+              <img className="w-12 h-12" src={instagram} alt="instagram" />
+            </a>
+          </div>
+        </div>
+      </p>
+      <div className="flex flex-col gap-8 px-4 md:px-12 m-12 w-full">
+        {projects.map((item) => (
+          <div
+            className={`flex flex-col rounded-lg bg-gray-200 shadow-xl ${
+              item.id % 2 !== 0 ? "md:flex-row" : "md:flex-row-reverse"
+            } gap-12 justify-evenly items-center p-8 `}
+            key={item.id}
+          >
+            <div className="md:w-1/2 self-start text-start  text-sm lg:text-2xl text-gray-800 italic">
+              {item.description.map((line, index) => (
+                <p className="my-2" key={index}>
+                  {line}
+                </p>
+              ))}
+            </div>
+            <ProjectSlider className="w-1/2" images={item.images} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
